43e69b9da985f9a891e1ba573adf5f84bc3bbea6
livenation12 <joshdionio21@gmail.com>
2022-12-21 18:38:16 -1200

    Update register.php

--- register.php
+++ register.php
@@ -13,30 +13,6 @@ $register = $class->register();
 	<title></title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
-<body>
-	<div class="container">	
-		<h1>Register</h1>
-	<form method="post">
-
-		<input required type="text" name="firstname" placeholder="firstname"><br>
-		<input required type="text" name="lastname" placeholder="lastname"><br>
-		<input required type="text" name="employee_id" placeholder="Employee ID"><br>
-		<input required type="text" name="contact" placeholder="contact"><br>
-		<input required type="text" name="department" placeholder="department"><br>
-		<input required type="text" name="dept_head_firstname" placeholder="Department/Office Head Firstname">
-			<br>
-		<input required type="text" name="dept_head_lastname" placeholder="Department/Office Head Lastname"><br>
-		<input required type="text" name="position" placeholder="Your Position"><br>
-		<input required type="password" name="password" placeholder="Set Your Password"><br>
-		<input required type="password" name="cpassword" placeholder="Confirm Your Password"><br>
-		<input required type="submit" name="register" value="REGISTER">
-	</form>
-</div>
-
-</body>
-</html>
-
-
 <section class="text-center text-lg-start">
   <style>
     body {
@@ -56,8 +32,7 @@ $register = $class->register();
     }
   </style>
 
-
-  <div class="container py-4">
+<div class="container py-4">
     <div class="row g-0 align-items-center">
       <div class="col-lg-6 mb-5 mb-lg-0">
         <div class="card cascading-right" style="
@@ -65,76 +40,94 @@ $register = $class->register();
             backdrop-filter: blur(30px);
             ">
           <div class="card-body p-5 shadow-5 text-center">
-            <h2 class="fw-bold mb-5">Sign up now</h2>
+            <h2 class="fw-bold mb-5">Register now!</h2>
             <form>
               <!-- 2 column grid layout with text inputs for the first and last names -->
               <div class="row">
                 <div class="col-md-6 mb-4">
                   <div class="form-outline">
-                    <input type="text" id="form3Example1" class="form-control" />
-                    <label class="form-label" for="form3Example1">First name</label>
+                  	<input required type="text" name="firstname" id="form3Example1" class="form-control" placeholder="firstname">
                   </div>
                 </div>
                 <div class="col-md-6 mb-4">
                   <div class="form-outline">
-                    <input type="text" id="form3Example2" class="form-control" />
-                    <label class="form-label" for="form3Example2">Last name</label>
+                  		<input required type="text" name="lastname" placeholder="lastname" id="form3Example2" class="form-control">
+               
+                  </div>
+                </div>
+              </div>
+               <div class="row">
+                <div class="col-md-6 mb-4">
+                  <div class="form-outline">
+                  	<input required type="text" name="employee_id" placeholder="Employee ID" class="form-control" placeholder="firstname">
+                  </div>
+                </div>
+                <div class="col-md-6 mb-4">
+                  <div class="form-outline">
+                  		<input required type="text" name="contact" placeholder="contact" id="form3Example2" class="form-control">
+                 
                   </div>
                 </div>
               </div>
 
-              <!-- Email input -->
-              <div class="form-outline mb-4">
-                <input type="email" id="form3Example3" class="form-control" />
-                <label class="form-label" for="form3Example3">Email address</label>
-              </div>
 
+                  </div>
+                </div>
+              </div>
+               <div class="row">
+                <div class="col-md-6 mb-4">
+                  <div class="form-outline">
+               
+                  	<input required type="text" name="dept_head_firstname" placeholder="Department/Office Head firstname" class="form-control" placeholder="Dept. Head firstname">
+                  </div>
+                </div>
+                <div class="col-md-6 mb-4">
+                  <div class="form-outline">
+                  		<input  required type="text" name="dept_head_lastname" placeholder="Department/Office Head Lastname" class="form-control">
+                 
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6 mb-4">
+                  <div class="form-outline">
+                  		<input  class="form-control">
+                  		<input required type="text" name="position" placeholder="Your Position" class="form-control">
+           
+                  </div>
+                </div>
+              </div>
+               
+               
+
+
+              <!-- Email input -->
+           
               <!-- Password input -->
               <div class="form-outline mb-4">
-                <input type="password" id="form3Example4" class="form-control" />
-                <label class="form-label" for="form3Example4">Password</label>
+              	<input required type="password" name="password" id="form3Example4" class="form-control" placeholder="Set Your Password">
+              </div>
+              <div class="form-outline mb-4">
+              	<input required type="password" name="cpassword" id="form3Example4" class="form-control" placeholder="Confirm Your Password">
               </div>
 
               <!-- Checkbox -->
-              <div class="form-check d-flex justify-content-center mb-4">
+            <!--   <div class="form-check d-flex justify-content-center mb-4">
                 <input class="form-check-input me-2" type="checkbox" value="" id="form2Example33" checked />
                 <label class="form-check-label" for="form2Example33">
                   Subscribe to our newsletter
                 </label>
               </div>
-
+ -->
               <!-- Submit button -->
-              <button type="submit" class="btn btn-primary btn-block mb-4">
+              		<button required type="submit" name="register" class="btn btn-primary btn-block mb-4">
                 Sign up
               </button>
-
-              <!-- Register buttons -->
-              <div class="text-center">
-                <p>or sign up with:</p>
-                <button type="button" class="btn btn-link btn-floating mx-1">
-                  <i class="fab fa-facebook-f"></i>
-                </button>
-
-                <button type="button" class="btn btn-link btn-floating mx-1">
-                  <i class="fab fa-google"></i>
-                </button>
-
-                <button type="button" class="btn btn-link btn-floating mx-1">
-                  <i class="fab fa-twitter"></i>
-                </button>
-
-                <button type="button" class="btn btn-link btn-floating mx-1">
-                  <i class="fab fa-github"></i>
-                </button>
-              </div>
+              	</form>
             </form>
           </div>
         </div>
       </div>
-
-      <div class="col-lg-6 mb-5 mb-lg-0">
-        <img src="https://mdbootstrap.com/img/new/ecommerce/vertical/004.jpg" class="w-100 rounded-4 shadow-4"
-          alt="" />
-      </div>
     </div>
-  </div>
+</div>
+</body>
+</html>
