e0bb024b117ee6cccf5d10720a6efae9c7fb37f7
JohnChryslerAlconera <acealconera@gmail.com>
2023-01-19 03:51:21 +1300

    oks na ni?

---
+++ adminnavbar.php
@@ -0,0 +1,50 @@
+<?php
+require_once("formclass.php");
+$userdetails = $class->get_userdata();
+$session = $class->sessionAdmin();
+
+
+
+  
+?>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+  <div class="container-fluid">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/e/e8/Seal_of_Iloilo_City.png" style="width:80px; height:70px;">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="mynavbar">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="addadmin.php">Add Admin</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="pendings.php">Pendings</a>
+        </li>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="approved.php">Approved</a>
+        </li>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="denied.php"> Denied</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="completed.php">Completed</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+      <!-- <a id="logout" href="logout.php"> <button>LOGOUT</button></a> -->
+        <a class="btn btn-primary" id="logout" href="logout.php"">Logout</a>
+      </form>
+    </div>
+  </div>
+</nav>
